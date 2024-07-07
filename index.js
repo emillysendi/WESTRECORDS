3ed6b1b63fba6c3c4de346630f0b7e0ecdf5ad20
Sendi <sendikaddiwaemilly@gmail.com>
2024-07-07 14:16:57 +0300

    arranged in two rows

---
+++ index.js
@@ -0,0 +1,14 @@
+//custom object
+let car = { 
+    make: "Toyota",
+    model: "corona",
+    year: 2022,
+    color: "silver",
+    start: function(){
+        console.log("The car has started");
+    }
+};
+console.log(car);
+console.log(car .make);
+let carMake = "Toyota" ;
+car.start();
