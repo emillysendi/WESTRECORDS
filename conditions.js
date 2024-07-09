3380bff5b221daa3f393e67d6087aba7cb49556e
Sendi <sendikaddiwaemilly@gmail.com>
2024-07-09 12:22:07 +0300

    added conditionals

---
+++ conditions.js
@@ -0,0 +1,35 @@
+//if
+//if else
+//if else if else
+//switch
+//ternary operation
+//if(condition){
+// //function
+//}
+let dayofTheWeekByUser=prompt("what is the date today")
+let day= dayofTheWeekByUser.toLowerCase()
+switch(day){   
+case "Monday":
+    console.log("Today is Monday")
+    break
+case"Tuesday":
+     console.log("Today is Tuesday")
+     break
+case "Wednsday":
+    console.log("Today is Wednsday")
+    break    
+case "Thursday":
+    console.log("Today is Thursday")
+    break    
+case "Friday":
+    console.log("Today is Friday")
+    break    
+case "Saturday":
+    console.log("Today is Sturday")
+    break             
+case "Sunday":
+    console.log("Today is Sunday")
+    break    
+   default:
+       console.log("NOT A DAY OF THE WEEK")
+}
